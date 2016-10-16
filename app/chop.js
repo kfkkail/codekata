f3e11f02f27abd3212c9d75eda0a86b2b405c502
Kail Keusch <kfkkail@gmail.com>
2016-10-16 10:37:21 -0400

    3rd version

--- app/chop.js
+++ app/chop.js
@@ -1,3 +1,5 @@
+//20ms
+//figure out quickest way to come up javascript way of solving
 exports.chop1 = function(int, array) {
 
   var chopped = array.indexOf(int);
@@ -6,13 +8,33 @@ exports.chop1 = function(int, array) {
 
 };
 
-exports.chop = function(int, array) {
+//20ms
+//start at the beginning of the array and work forwards
+exports.chop2 = function(int, array) {
 
   var chopped = -1;
 
   for(i = 0; i<array.length; i++) {
     if (array[i] === int) {
       chopped = i;
+      break;
+    }
+  }
+
+  return chopped;
+};
+
+//20ms
+//start at the end of the array and work backwards
+exports.chop = function(int, array) {
+
+  var chopped = -1;
+  var index;
+
+  for(i = 0; i<array.length; i++) {
+    index = array.length-1-i;
+    if (array[index] === int) {
+      chopped = index;
       break;
     }
   }
